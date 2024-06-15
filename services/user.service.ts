acec7af62f14412060f124fe7b4ac1a66b1a83cd
sagar557 <choudharysagar215@gmail.com>
2024-06-15 13:27:08 +0530

    Fixed n

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,5 +1,5 @@
 import { Response } from "express";
-import { redis } from "../utils/redis";
+import  redis  from "../utils/redis";
 import userModel from "../models/user.model";
 
 export const getUserById = async (id: string, res: Response) => {
