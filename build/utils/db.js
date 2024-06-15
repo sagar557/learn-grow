f26d05ecf3229677bdc6aa6b71d84e204f23271a
sagar557 <choudharysagar215@gmail.com>
2024-06-15 14:15:48 +0530

    Fixed build n

--- build/utils/db.js
+++ build/utils/db.js
@@ -4,17 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-require("dotenv").config();
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const dbUrl = process.env.DB_URL || '';
 const connectDB = async () => {
     try {
-        await mongoose_1.default.connect(dbUrl).then((data) => {
-            console.log(`Database Connected Successfully with ${data.connection.host}`);
+        await mongoose_1.default.connect(dbUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
         });
+        console.log(`Database Connected Successfully with ${mongoose_1.default.connection.host}`);
     }
     catch (error) {
-        console.log("Error while connecting to the database", error.message);
-        setTimeout(connectDB, 5000); //try again after 5 seconds
+        console.error("Error connecting to MongoDB:", error.message);
+        setTimeout(connectDB, 5000); // try again after 5 seconds
     }
 };
 exports.default = connectDB;
--- utils/db.ts
+++ utils/db.ts
@@ -1,16 +1,24 @@
 import mongoose from "mongoose";
-require ("dotenv").config();
-const dbUrl:string = process.env.DB_URL || '';
+import dotenv from "dotenv";
 
-const connectDB =async () => {
-    try{
-        await mongoose.connect(dbUrl).then((data:any)=>{
-            console.log(`Database Connected Successfully with ${data.connection.host}`);
-        });
-    }   catch(error:any){
-        console.log("Error while connecting to the database", error.message);
-        setTimeout(connectDB, 5000); //try again after 5 seconds
-    } 
-}
+dotenv.config();
 
-export default  connectDB;
+const dbUrl: string = process.env.DB_URL || '';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        } as mongoose.ConnectOptions);
+
+        console.log(`Database Connected Successfully with ${mongoose.connection.host}`);
+    } catch (error: any) {
+        console.error("Error connecting to MongoDB:", error.message);
+        setTimeout(connectDB, 5000); // try again after 5 seconds
+    }
+};
+
+export default connectDB;
