74007b56e27f4b490bb33514535d868e58b03667
sagar557 <choudharysagar215@gmail.com>
2024-06-16 10:42:11 +0530

    Fixed nnn

--- build/utils/redis.js
+++ build/utils/redis.js
@@ -1,24 +1,8 @@
 "use strict";
-// import Redis from "ioredis";
-// import dotenv from 'dotenv';
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// dotenv.config();
-// const redisClient = () => {
-//     if (process.env.REDIS_URL) {
-//         console.log('Redis is connected');
-//         return new Redis(process.env.REDIS_URL, {
-//             retryStrategy(times) {
-//                 return Math.min(times * 50, 2000);
-//             },
-//             maxRetriesPerRequest: 50,
-//         });
-//     }
-//     throw new Error("Redis connection failed");
-// };
-// export const redis = redisClient();
 const ioredis_1 = __importDefault(require("ioredis"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
--- utils/redis.ts
+++ utils/redis.ts
@@ -1,22 +1,3 @@
-// import Redis from "ioredis";
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const redisClient = () => {
-//     if (process.env.REDIS_URL) {
-//         console.log('Redis is connected');
-//         return new Redis(process.env.REDIS_URL, {
-//             retryStrategy(times) {
-//                 return Math.min(times * 50, 2000);
-//             },
-//             maxRetriesPerRequest: 50,
-//         });
-//     }
-//     throw new Error("Redis connection failed");
-// };
-
-// export const redis = redisClient();
 import Redis from 'ioredis';
 import dotenv from 'dotenv';
 
